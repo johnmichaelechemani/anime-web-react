2a8251eddb351e136e0146c960174ab07dac5cc3
Your Name <deudajm@gmail.com>
2024-07-19 17:35:22 +0800

    start: set ups

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./style/app.css";
+
+function App() {
+  return (
+    <>
+      <div className="">
+        {" "}
+        <h1>Hi</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
