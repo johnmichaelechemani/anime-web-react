8ca5fb975ea04a1dce96c1068384f59e247c5b56
Your Name <deudajm@gmail.com>
2024-07-23 15:08:55 +0800

    new ups

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,21 @@ import "./styles/app.css";
 function App() {
   return (
     <>
-      <div className="">
+      <div className="m-10">
         {" "}
-        <h1 className="font-extrabold text-5xl">Hi</h1>
+        <input
+          type="search"
+          placeholder="Search.."
+          className="input input-bordered rounded-full w-full max-w-xs"
+        />
+        <div className="py-5 flex justify-start items-center gap-3">
+          <button className="btn btn-sm rounded-full btn-primary px-6">
+            All
+          </button>
+          <button className="btn btn-sm rounded-full ">Playlist</button>
+          <button className="btn btn-sm rounded-full ">New</button>
+          <button className="btn btn-sm rounded-full ">Small</button>
+        </div>
       </div>
     </>
   );
