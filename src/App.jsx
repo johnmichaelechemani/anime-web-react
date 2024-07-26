5e095e0c7610043d851c97ac7a42593721d728d6
Your Name <deudajm@gmail.com>
2024-07-26 16:03:58 +0800

    "Wrapped nav items in a div with overflow-x-scroll and no-scrollbar classes, and added CSS to hide scrollbar for nav items"

--- src/App.jsx
+++ src/App.jsx
@@ -114,22 +114,24 @@ function App() {
           )}
         </div>
         <div className="py-5 pt-20 sm:pt-10 flex justify-start px-8 items-center gap-3">
-          {navItem.map((item) => {
-            return (
-              <button
-                key={item.id}
-                onClick={item.action}
-                className={`btn btn-sm rounded-full uppercase px-6 ${
-                  activeCategory === item.name
-                    ? "btn-primary text-sm "
-                    : "bg-transparent text-xs border border-gray-500/50"
-                }`}
-              >
-                {" "}
-                {item.name}
-              </button>
-            );
-          })}
+          <div className="flex justify-start items-center gap-3 overflow-hidden overflow-x-scroll no-scrollbar">
+            {navItem.map((item) => {
+              return (
+                <button
+                  key={item.id}
+                  onClick={item.action}
+                  className={`btn btn-sm rounded-full uppercase px-6 ${
+                    activeCategory === item.name
+                      ? "btn-primary text-sm "
+                      : "bg-transparent text-xs border border-gray-500/50"
+                  }`}
+                >
+                  {" "}
+                  {item.name}
+                </button>
+              );
+            })}
+          </div>
         </div>
         <div className="flex justify-end items-center px-8 my-2">
           <div className="join">
