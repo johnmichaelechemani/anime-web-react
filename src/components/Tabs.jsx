6cca83021347ce2e36b5a9c0be60ab737363807f
Your Name <deudajm@gmail.com>
2024-07-28 14:38:48 +0800

    "Updated Tabs component: modified CSS classes and added two absolute div elements with gradients."

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -3,8 +3,8 @@ import PropTypes from "prop-types";
 export default function Tabs({ navItem, activeCategory }) {
   return (
     <>
-      <div className="py-5 pt-20 sm:pt-10 flex justify-start px-8 items-center gap-3">
-        <div className="flex justify-start items-center gap-3 overflow-hidden overflow-x-scroll no-scrollbar">
+      <div className="py-5 pt-20 sm:pt-10 flex justify-start px-8 items-center gap-3 relative">
+        <div className="flex justify-start items-center gap-3 px-5 overflow-hidden overflow-x-scroll no-scrollbar relative">
           {navItem.map((item) => {
             return (
               <button
@@ -22,6 +22,8 @@ export default function Tabs({ navItem, activeCategory }) {
             );
           })}
         </div>
+        <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3  bg-gradient-to-r from-base-100"></div>
+        <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-base-100"></div>
       </div>
     </>
   );
