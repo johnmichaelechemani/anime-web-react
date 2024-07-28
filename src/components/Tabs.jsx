913a0236e121818d531d1d825bcf3529d0f14c21
Your Name <deudajm@gmail.com>
2024-07-28 14:40:04 +0800

    "Remove px-5 from inner div and two absolute divs with gradients"

--- src/components/Tabs.jsx
+++ src/components/Tabs.jsx
@@ -4,7 +4,7 @@ export default function Tabs({ navItem, activeCategory }) {
   return (
     <>
       <div className="py-5 pt-20 sm:pt-10 flex justify-start px-8 items-center gap-3 relative">
-        <div className="flex justify-start items-center gap-3 px-5 overflow-hidden overflow-x-scroll no-scrollbar relative">
+        <div className="flex justify-start items-center gap-3 overflow-hidden overflow-x-scroll no-scrollbar">
           {navItem.map((item) => {
             return (
               <button
@@ -22,8 +22,6 @@ export default function Tabs({ navItem, activeCategory }) {
             );
           })}
         </div>
-        <div className="pointer-events-none absolute inset-y-0 left-0 w-1/3  bg-gradient-to-r from-base-100"></div>
-        <div className="pointer-events-none absolute inset-y-0 right-0 w-1/3 bg-gradient-to-l from-base-100"></div>
       </div>
     </>
   );
